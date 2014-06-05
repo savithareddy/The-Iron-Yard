a564bb3332a9651220a55d9708b1148cac7c3eb7
savitha_ios <reddy.savi@gmail.com>
2014-06-05 00:41:12 -0400

    Push Notification

---
+++ TestPush/TestPush/TPHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TPHAppDelegate.h
+//  TestPush
+//
+//  Created by Savitha Reddy on 6/2/14.
+//  Copyright (c) 2014 Savitha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestPush/TestPush/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestPush
+//
+//  Created by Savitha Reddy on 6/2/14.
+//  Copyright (c) 2014 Savitha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TPHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TPHAppDelegate class]));
+    }
+}
