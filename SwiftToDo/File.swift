c259fb048416261ec34bd7fff5b3cc51f9f9fb98
savitha_ios <reddy.savi@gmail.com>
2014-06-12 12:38:03 -0400

    Swift New Language Table View

---
+++ SwiftToDo/File.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  SwiftToDo
+//
+//  Created by Savitha Reddy on 6/12/14.
+//  Copyright (c) 2014 Savitha. All rights reserved.
+//
+
+import Foundation
+
+//class RootViewController:UIViewController{ // this was UIKIt and UIVIewController so auto populates objects of this class like UIView and Others 
+//    
+//    override func viewDidLoad()
+//    {
+//        super.viewDidLoad()
+//        //        println("hello")
+//        
+//        //        var box : UIView = UIView() // : for the function
+//        //        box.frame = CGRectMake(50, 50, 100, 100)
+//        ////        box = "box" // once variable is declared to be UIView cannot be changed to String  or other
+//        
+//        var box = UIView()
+//        box.frame = CGRectMake(50, 50, 100, 100)
+//        //         box.backgroundColor = UIColor.greenColor().colorWithAlphaComponent(0.5)
+//        var grey = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5) //for single parameters direct value can be input but for many arguments name :  is required 
+//        box.backgroundColor = grey
+//        self.view.addSubview(box)
+//        
+//        
+//    }
+//}
+
