606bc44aab06b2b462688cf1cdf142007c951179
savitha_ios <reddy.savi@gmail.com>
2014-07-17 17:30:39 -0400

    Changes

---
+++ NavToolBar/NTBTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  NTBTableViewController.h
+//  NavToolBar
+//
+//  Created by Savitha Reddy on 5/17/14.
+//  Copyright (c) 2014 Savitha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTBTableViewController : UITableViewController
+
+-(id) colorsNumbers: (NSInteger) value1;
+@property (nonatomic) NSInteger givalue;
+
+
+@end
