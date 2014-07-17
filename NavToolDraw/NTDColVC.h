729880b87e4bbc91e0460b0f9b62763cb1e41b3f
savitha_ios <reddy.savi@gmail.com>
2014-07-17 17:34:42 -0400

    Changes

---
+++ NavToolDraw/NTDColVC.h
@@ -0,0 +1,13 @@
+//
+//  NTDColVC.h
+//  NavToolDraw
+//
+//  Created by Savitha Reddy on 5/16/14.
+//  Copyright (c) 2014 Savitha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTDColVC : UIViewController
+
+@end
